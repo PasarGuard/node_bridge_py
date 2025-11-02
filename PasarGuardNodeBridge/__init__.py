4c0fbd2004a3e01a2f71e2f9c73b7d9c2e961e5f
M03ED <50927468+M03ED@users.noreply.github.com>
2025-11-02 12:52:24 +0330

    bump to v0.1.1

--- PasarGuardNodeBridge/__init__.py
+++ PasarGuardNodeBridge/__init__.py
@@ -12,10 +12,10 @@ Features:
 - Extensible with custom metadata via the `extra` argument
 
 Author: PasarGuard
-Version: 0.1.0
+Version: 0.1.1
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "PasarGuard"
 
 
